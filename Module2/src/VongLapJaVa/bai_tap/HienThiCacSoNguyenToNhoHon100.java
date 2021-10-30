39d92a768f00374207886daaa2b6f8ce77510c6e
Your Name <doantai100983hu@gmail.com>
2021-10-30 17:50:48 +0700

    up bai

---
+++ Module2/src/VongLapJaVa/bai_tap/HienThiCacSoNguyenToNhoHon100.java
@@ -0,0 +1,38 @@
+package VongLapJaVa.bai_tap;
+
+import java.util.Scanner;
+
+public class HienThiCacSoNguyenToNhoHon100 {
+    public static boolean snt(int n){
+            if (n < 2 ){
+                return false;
+            }
+            for (int i = 2 ; i < n ; i++){
+                    if (n % i == 0){
+                        return false ;
+                    }
+            }
+            return true ;
+    }
+
+    public static void main(String[] args) {
+        int number = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+        do {
+            System.out.println("Mời nhập số từ 2 -> 100");
+             number = sc.nextInt();
+        }while ((number < 2) || (number >100));
+
+        for (int i = 0 ; i < number ; i++){
+            if (snt(i)){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+    }
+
